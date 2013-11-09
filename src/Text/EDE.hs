b601c344bcebc63893d539fdae24902d5c754db1
Brendan Hay <brendan.g.hay@gmail.com>
2013-11-09 17:17:07 +0100

    Adding license headers

--- src/Text/EDE.hs
+++ src/Text/EDE.hs
@@ -1,5 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- Module      : Text.EDE
+-- Copyright   : (c) 2013 Brendan Hay <brendan.g.hay@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+
+-- |
 module Text.EDE where
 
 import           Data.Aeson                    (Object, Value(..), (.=), object)
--- src/Text/EDE/Internal/Parser.hs
+++ src/Text/EDE/Internal/Parser.hs
@@ -1,5 +1,15 @@
 {-# LANGUAGE TupleSections #-}
 
+-- Module      : Text.EDE.Internal.Parser
+-- Copyright   : (c) 2013 Brendan Hay <brendan.g.hay@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+
 module Text.EDE.Internal.Parser where
 
 import           Control.Applicative     ((<$>), (<*>), (<*), (*>), pure)
