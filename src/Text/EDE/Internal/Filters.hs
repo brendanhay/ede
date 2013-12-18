cc2a09711dea36d6f203de85e8c4c6692dceeb83
Brendan Hay <brendan.g.hay@gmail.com>
2013-12-18 16:03:08 +0100

    Adding additional filters

--- src/Text/EDE/Internal/Filters.hs
+++ src/Text/EDE/Internal/Filters.hs
@@ -15,17 +15,35 @@ module Text.EDE.Internal.Filters
     ( defaults
     ) where
 
-import           Data.Char               (toLower, toUpper)
-import           Data.HashMap.Strict     (HashMap)
+import           Data.Char
 import qualified Data.HashMap.Strict     as Map
+import           Data.HashMap.Strict     (HashMap)
 import           Data.Text               (Text)
 import qualified Data.Text               as Text
+import qualified Data.Vector             as Vector
 import           Text.EDE.Internal.Types
 
 -- FIXME: Create polymorphic filters
-
 defaults :: HashMap Text Fun
 defaults = Map.fromList
-    [ ("lower", Fun TText TText $ Text.map toLower)
-    , ("upper", Fun TText TText $ Text.map toUpper)
+    [ ("lower",      Fun TText TText $ Text.map toLower)
+    , ("upper",      Fun TText TText $ Text.map toUpper)
+    , ("lowerFirst", Fun TText TText $ lowerFirst)
+    , ("upperFirst", Fun TText TText $ upperFirst)
+    , ("listLength", Fun TList TInt  $ fromIntegral . Vector.length)
+    , ("mapLength",  Fun TMap TInt   $ fromIntegral . Map.size)
     ]
+  where
+    lowerFirst t
+        | Text.null t = t
+        | isUpper h   = toLower h `Text.cons` Text.tail t
+        | otherwise   = t
+      where
+        h = Text.head t
+
+    upperFirst t
+        | Text.null t = t
+        | isLower h   = toUpper h `Text.cons` Text.tail t
+        | otherwise   = t
+      where
+        h = Text.head t
