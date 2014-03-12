a5b9d3ed0e2e37219b72a636c900b6681560442c
Brendan Hay <brendan.g.hay@gmail.com>
2014-03-12 10:50:49 +0100

    Adding re-worked expression tree

--- src/Text/EDE/Internal/Types.hs
+++ src/Text/EDE/Internal/Types.hs
@@ -1,10 +1,6 @@
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE KindSignatures             #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE DeriveFoldable    #-}
+{-# LANGUAGE DeriveFunctor     #-}
+{-# LANGUAGE DeriveTraversable #-}
 
 -- Module      : Text.EDE.Internal.Types
 -- Copyright   : (c) 2013-2014 Brendan Hay <brendan.g.hay@gmail.com>
@@ -18,147 +14,56 @@
 
 module Text.EDE.Internal.Types where
 
-import           Control.Applicative
-import           Data.HashSet        (HashSet)
-import qualified Data.HashSet        as Set
-import           Data.Hashable
-import           Data.Monoid
-import           Data.Text           (Text)
-
-data Meta = Meta
-    { metaName :: String
-    , metaRow  :: Int
-    , metaCol  :: Int
-    } deriving (Eq)
-
-instance Show Meta where
-    show Meta{..} = metaName <> ":" <> show metaRow <> ":" <> show metaCol
-
-data Ann a = Ann
-    { annType :: Polytype
-    , annTail :: a
-    } deriving (Show)
-
-type Id = Text
-
-data Var
-    = VBound { varId :: Id }
-    | VFree  { varId :: Id }
-      deriving (Eq, Show)
-
-newtype Bind = Bind Id deriving (Eq, Show)
-
-bind :: Bind -> Var
-bind (Bind i) = VBound i
-
-data Lit
-    = LNum  Integer
-    | LText Text
-    | LBool Bool
-      deriving (Eq, Show)
+import Bound
+import Control.Applicative
+import Control.Monad
+import Data.Foldable
+import Data.Traversable
+import Prelude.Extras
 
 data Exp a
-    = ELit a Lit
-    | EVar a Var
-    | EAbs a Bind    (Exp a)
-    | EApp a (Exp a) (Exp a)
-    | ELet a Bind    (Exp a) (Exp a)
-      deriving (Eq, Show)
+    = EVar  a
+    | EApp  (Exp a) (Exp a)
+    | ELam  !Int    (Pat Exp a)       (Scope Int Exp a)
+    | ELet  !Int    [Scope Int Exp a] (Scope Int Exp a)
+    | ECase (Exp a) [Alt Exp a]
+      deriving (Eq, Ord, Show, Read, Functor, Foldable, Traversable)
 
-instance Functor Exp where
-    fmap f (ELit x l)     = ELit (f x) l
-    fmap f (EVar x v)     = EVar (f x) v
-    fmap f (EAbs x b e)   = EAbs (f x) b (f <$> e)
-    fmap f (EApp x a b)   = EApp (f x)   (f <$> a) (f <$> b)
-    fmap f (ELet x b r e) = ELet (f x) b (f <$> r) (f <$> e)
+instance Applicative Exp where
+    pure  = EVar
+    (<*>) = ap
 
-data Pat
-    = PWildcard
-    | PVar Id
-    | PLit Lit
-      deriving (Eq, Show)
+instance Monad Exp where
+    EVar  a      >>= f = f a
+    EApp  x y    >>= f = EApp (x >>= f) (y >>= f)
+    ELam  n p e  >>= f = ELam n (p >>>= f) (e >>>= f)
+    ELet  n bs e >>= f = ELet n (map (>>>= f) bs) (e >>>= f)
+    ECase e as   >>= f = ECase (e >>= f) (map (>>>= f) as)
 
-data TCon
-    = TNum
-    | TText
-    | TBool
-      deriving (Eq, Show)
+    return = EVar
 
-newtype TVar = TypeVar Id deriving (Eq, Ord, Show, Hashable)
+instance Eq1   Exp
+instance Ord1  Exp
+instance Show1 Exp
+instance Read1 Exp
 
-data TKind = Mono | Poly
+data Pat f a
+    = PVar
+    | PWild
+    | PAs   (Pat f a)
+    | PCon  String [Pat f a]
+    | PView (Scope Int f a) (Pat f a)
+      deriving (Eq, Ord, Show, Read, Functor, Foldable, Traversable)
 
--- | Types, indexed by their kind: Monotype or Polytype.
---   Only Polytypes can have foralls.
-data Type :: TKind -> * where
-    TCon    :: TCon   -> Type a
-    TVar    :: TVar   -> Type a                 -- ^ alpha
-    TExists :: TVar   -> Type a                 -- ^ alpha^
-    TForall :: TVar   -> Type Poly -> Type Poly -- ^ forall alpha. A
-    TFun    :: Type a -> Type a    -> Type a    -- ^ A -> B
+instance Bound Pat where
+    PVar       >>>= _ = PVar
+    PWild      >>>= _ = PWild
+    PAs   p    >>>= f = PAs (p >>>= f)
+    PCon  g ps >>>= f = PCon g (map (>>>= f) ps)
+    PView e p  >>>= f = PView (e >>>= f) (p >>>= f)
 
-deriving instance Show (Type a)
-deriving instance Eq   (Type a)
+data Alt f a = Alt !Int (Pat f a) (Scope Int f a)
+    deriving (Eq, Ord, Show, Read, Functor, Foldable, Traversable)
 
-type Polytype = Type Poly
-type Monotype = Type Mono
-
--- | Is the type a Monotype?
-monotype :: Type a -> Maybe Monotype
-monotype typ = case typ of
-    TCon c      -> Just $ TCon c
-    TVar v      -> Just $ TVar v
-    TForall _ _ -> Nothing
-    TExists v   -> Just $ TExists v
-    TFun t1 t2  -> TFun <$> monotype t1 <*> monotype t2
-
--- | Any type is a Polytype since Monotype is a subset of Polytype
-polytype :: Type a -> Polytype
-polytype typ = case typ of
-    TCon c      -> TCon c
-    TVar v      -> TVar v
-    TForall v t -> TForall v t
-    TExists v   -> TExists v
-    TFun t1 t2  -> TFun (polytype t1) (polytype t2)
-
--- | The free type variables in a type
-freeTVars :: Type a -> HashSet TVar
-freeTVars typ = case typ of
-    TCon _      -> mempty
-    TVar v      -> Set.singleton v
-    TForall v t -> Set.delete v (freeTVars t)
-    TExists v   -> Set.singleton v
-    TFun t1 t2  -> freeTVars t1 <> freeTVars t2
-
-data Elem
-    = CVar          Var  Polytype -- ^ x : A
-    | CForall       TVar          -- ^ alpha
-    | CExists       TVar          -- ^ alpha^
-    | CExistsSolved TVar Monotype -- ^ alpha^ = tau
-    | CMarker       TVar          -- ^ |> alpha^
-      deriving (Eq, Show)
-
-newtype Context = Context [Elem]
-    deriving (Show)
-
-instance Monoid Context where
-    mempty = Context []
-    mappend (Context a) (Context b) = Context (b ++ a)
-
-class Metadata a where
-    meta :: a -> Meta
-
-instance Metadata Meta where
-    meta = id
-
-instance Metadata a => Metadata (Ann a) where
-    meta = meta . annTail
-
-instance Metadata a => Metadata (Exp a) where
-    meta = meta . ann
-      where
-        ann (ELit a _)     = a
-        ann (EVar a _)     = a
-        ann (EAbs a _ _)   = a
-        ann (EApp a _ _)   = a
-        ann (ELet a _ _ _) = a
+instance Bound Alt where
+    Alt n p b >>>= f = Alt n (p >>>= f) (b >>>= f)
