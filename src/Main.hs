7db17acdbc42a873d851e7c7e33885091c9bd293
Brendan Hay <brendan.g.hay@gmail.com>
2014-10-08 08:25:01 +0200

    Updating formatting and licensing

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,31 @@
 {-# LANGUAGE LambdaCase      #-}
 {-# LANGUAGE RecordWildCards #-}
 
+-- Module      : Text.EDE
+-- Copyright   : (c) 2013-2014 Brendan Hay <brendan.g.hay@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Brendan Hay <brendan.g.hay@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+
 module Main (main) where
 
 import           Control.Monad              (unless)
 import           Data.Aeson
-import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as LBS
 import           Data.Conduit
-import qualified Data.Conduit.Attoparsec    as C
-import qualified Data.Conduit.Binary        as C
-import qualified Data.Text.Lazy.IO          as T
+import qualified Data.Conduit.Attoparsec    as Conduit
+import qualified Data.Conduit.Binary        as Conduit
+import qualified Data.Text.Lazy.IO          as Text
 import           Data.Version               (showVersion)
 import           Options.Applicative
 import           Paths_ede                  (version)
 import           System.Directory           (getPermissions, readable)
 import           System.IO                  (hSetBinaryMode, stdin)
 import qualified Text.EDE                   as EDE
-
 
 data Opts = Opts
     { template :: FilePath
@@ -43,7 +52,7 @@ parseOpts = Opts
           )
         )
   where
-    readJSON = either Left jObject . eitherDecode . B.pack
+    readJSON = either Left jObject . eitherDecode . LBS.pack
 
 optInfo :: ParserInfo Opts
 optInfo = info (helper <*> parseOpts)
@@ -56,7 +65,7 @@ main :: IO ()
 main = execParser optInfo >>= \ Opts{..} -> do
     t <- readTemplate template
     b <- maybe fromStdin return bindings
-    EDE.result errRender T.putStrLn $ EDE.render t b
+    EDE.result errRender Text.putStrLn $ EDE.render t b
   where
     readTemplate t = do
         p <- getPermissions t
@@ -65,12 +74,13 @@ main = execParser optInfo >>= \ Opts{..} -> do
 
     fromStdin = do
         hSetBinaryMode stdin True
-        j <- C.sourceHandle stdin $$ C.sinkParser json'
+        j <- Conduit.sourceHandle stdin $$ Conduit.sinkParser json'
         either error pure $ jObject j
 
-    errRender m  es = error . unlines $ "Error rendering template:" : show m : es
-    errUnreadable t = error $ t ++ " is not readable"
+    errRender m es = error . unlines $
+        "Error rendering template:" : show m : es
 
+    errUnreadable t = error $ t ++ " is not readable"
 
 jObject :: Value -> Either String Object
 jObject (Object o) = Right o
