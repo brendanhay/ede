d7ae4e95de5dbc2fc61f4cf2a4d0a151fbf03cc2
Brendan Hay <brendan.g.hay@gmail.com>
2020-11-28 14:27:59 +0100

    Reformatting

--- app/ede.hs
+++ app/ede.hs
@@ -1,128 +1,138 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
 import Control.Applicative ((<*>))
 import qualified Data.Aeson as JSON
-import qualified Data.Attoparsec.ByteString as P
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Attoparsec.ByteString as Parsec
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Lazy as ByteString.Lazy
 import Data.Functor ((<$>))
-import qualified Data.HashMap.Strict as Map
-import qualified Data.Text as Text (pack)
-import qualified Data.Text.Encoding as Text (encodeUtf8)
-import qualified Data.Text.Lazy.IO as Text (putStr)
-import Options.Applicative (mappend)
-import qualified Options.Applicative as Opt
-import Options.Applicative.Help.Pretty
+import qualified Data.HashMap.Strict as HashMap
+import Data.Semigroup ((<>))
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text.Encoding
+import qualified Data.Text.Lazy.IO as Text.Lazy.IO
+import qualified Options.Applicative as Options
+import qualified Options.Applicative.Help.Pretty as Pretty
 import qualified Text.EDE as EDE
+
+data Options = Options
+  { templateFile :: FilePath,
+    jsonFile :: Maybe FilePath,
+    jsonObject :: Maybe Aeson.Object
+  }
+
+optionsParser :: Options.Parser Options
+optionsParser =
+  Options
+    <$> Options.strOption
+      ( Options.long "template-file"
+          <> Options.metavar "PATH"
+          <> Options.help
+            "Path of a file containing an EDE template"
+      )
+    <*> Options.optinal
+      ( Options.option
+          readObject
+          ( Options.long "context-json"
+              <> Options.metavar "OBJECT"
+              <> Options.help
+                "Template context as a JSON object"
+          )
+      )
+    <*> Options.optinal
+      ( Options.strOption
+          ( Options.long "context-file"
+              <> Options.metavar "PATH"
+              <> Options.help
+                "Path of a file containing a JSON object which is used \
+                \as the template context"
+          )
+      )
+
+parserInfo :: Options.ParserInfo Options
+parserInfo =
+  Options.info
+    (Options.helper <*> optionsParser)
+    (Options.header "EDE template processor" <> Options.progDescDoc (Just usage))
+  where
+    usage =
+      Pretty.vcat
+        [ Pretty.empty,
+          "The --context-file and --context-json options are processed as follows:",
+          Pretty.indent 2 "1.)"
+            <+> ( Pretty.align
+                    ( "Both are provided"
+                        Pretty.<$$> "Both objects are merged into one with the keys of \
+                                    \--context-json taking precedence over those in the \
+                                    \file provided by --context-file."
+                    )
+                ),
+          Pretty.indent 2 "2.)"
+            <+> ( Pretty.align
+                    ( "None of them are provided"
+                        Pretty.<$$> "The JSON object is read from STDIN."
+                    )
+                ),
+          Pretty.indent 2 "3.)"
+            <+> ( Pretty.align
+                    ( "One of them is provided"
+                        Pretty.<$$> "The JSON object is read from the supplied option."
+                    )
+                )
+        ]
 
 main :: IO ()
 main = do
-  opts <- getOpts
-  ctx <- case (jsonFile opts, jsonObject opts) of
-    (Just fp, Just obj1) -> do
-      eobj2 <- JSON.eitherDecode <$> LBS.readFile fp
-      case eobj2 of
-        Left e -> fail e
-        Right obj2 -> return $ Map.union obj1 obj2
-    (Just fp, Nothing) -> do
-      eobj <- JSON.eitherDecode <$> LBS.readFile fp
-      case eobj of
-        Left e -> fail e
-        Right obj -> return obj
-    (Nothing, Just obj) -> return obj
-    (Nothing, Nothing) -> BS.getContents >>= either fail return . P.parseOnly stdinParser
-  tplResult <- EDE.parseFile (templateFile opts)
-  case tplResult of
-    EDE.Failure err -> print err
+  options <- Options.execParser parserInfo
+
+  ctx <-
+    case (jsonFile options, jsonObject options) of
+      (Just path, Just obj1) -> do
+        eobj2 <- Aeson.eitherDecode <$> LBS.readFile path
+
+        case eobj2 of
+          Left err -> fail err
+          Right obj2 -> pure (Map.union obj1 obj2)
+      --
+      (Just path, Nothing) -> do
+        eobj <- Aeson.eitherDecode <$> LBS.readFile path
+        case eobj of
+          Left err -> fail err
+          Right obj -> pure obj
+      --
+      (Nothing, Just obj) ->
+        pure obj
+      --
+      (Nothing, Nothing) ->
+        BS.getContents
+          >>= either fail pure . Parsec.parseOnly stdinParser
+
+  EDE.parseFile (templateFile options) >>= \case
+    EDE.Failure err -> print err >> Exit.exitFailure
     EDE.Success tpl ->
       case EDE.render tpl ctx of
-        EDE.Failure err -> print err
-        EDE.Success output -> Text.putStr output
+        EDE.Failure err -> print err >> Exit.exitFailure
+        EDE.Success output -> Text.Lazy.IO.putStr output
 
-stdinParser :: P.Parser JSON.Object
-stdinParser = JSON.json >>= requireJsonObj
+stdinParser :: Parsec.Parser Aeson.Object
+stdinParser = Aeson.json >>= requireObject
 
-data Opts = Opts
-  { templateFile :: FilePath,
-    jsonFile :: Maybe FilePath,
-    jsonObject :: Maybe JSON.Object
-  }
+readValue :: Options.ReadM Aeson.Value
+readValue = Options.str >>= decodeJsonStr
 
-optsParser :: Opt.Parser Opts
-optsParser =
-  Opts
-    <$> templateFileParser
-    <*> Opt.optional jsonFileParser
-    <*> Opt.optional jsonParser
-
-templateFileParser :: Opt.Parser FilePath
-templateFileParser =
-  Opt.strOption $
-    Opt.long "template-file"
-      `mappend` Opt.metavar "PATH"
-      `mappend` Opt.help "Path of a file containing an EDE template"
-
-jsonParser :: Opt.Parser JSON.Object
-jsonParser =
-  Opt.option readJsonObj $
-    Opt.long "context-json"
-      `mappend` Opt.metavar "OBJECT"
-      `mappend` Opt.help "Template context as a JSON object"
-
-jsonFileParser :: Opt.Parser FilePath
-jsonFileParser =
-  Opt.strOption $
-    Opt.long "context-file"
-      `mappend` Opt.metavar "PATH"
-      `mappend` Opt.help "Path of a file containing a JSON object which is used as the template context"
-
-readJsonVal :: Opt.ReadM JSON.Value
-readJsonVal = Opt.str >>= decodeJsonStr
-
-decodeJsonStr :: Monad m => String -> m JSON.Value
+decodeJsonStr :: Monad m => String -> m Aeson.Value
 decodeJsonStr =
-  either fail return . JSON.eitherDecode . LBS.fromStrict
+  either fail pure . Aeson.eitherDecode . LBS.fromStrict
     . Text.encodeUtf8
     . Text.pack
 
-readJsonObj :: Opt.ReadM JSON.Object
-readJsonObj = readJsonVal >>= requireJsonObj
+readObject :: Options.ReadM Aeson.Object
+readObject = readValue >>= requireObject
 
-requireJsonObj :: Monad m => JSON.Value -> m JSON.Object
-requireJsonObj (JSON.Object obj) = return obj
-requireJsonObj _ = fail "JSON value must be an object"
-
-optsParserInfo :: Opt.ParserInfo Opts
-optsParserInfo =
-  Opt.info (Opt.helper <*> optsParser) $
-    Opt.header "EDE template processor"
-      `mappend` Opt.progDescDoc
-        ( Just $
-            vcat
-              [ empty,
-                "The --context-file and --context-json options are processed as follows:",
-                indent 2 "1.)"
-                  <+> ( align
-                          ( "Both are provided"
-                              <$$> "Both objects are merged into one with the keys of --context-json taking precedence over those in the file provided by --context-file."
-                          )
-                      ),
-                indent 2 "2.)"
-                  <+> ( align
-                          ( "None of them is provided"
-                              <$$> "The JSON object is read from STDIN."
-                          )
-                      ),
-                indent 2 "3.)"
-                  <+> ( align
-                          ( "One of them is provided"
-                              <$$> "The obvious thing happens."
-                          )
-                      )
-              ]
-        )
-
-getOpts :: IO Opts
-getOpts = Opt.execParser optsParserInfo
+requireObject :: Monad m => Aeson.Value -> m Aeson.Object
+requireObject = \case
+  Aeson.Object obj -> pure obj
+  _ -> fail "JSON value must be an object"
